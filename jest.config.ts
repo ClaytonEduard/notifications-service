b11d4521d18f03aefc4adca55921c5eb8ece0d43
ClaytonEduard <clayton_eduard@hotmail.com>
2022-12-14 23:38:14 -0300

    :bug: corrigindo

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,14 @@
-export default {
+import { Config } from 'jest'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+const config: Config = {
     "moduleFileExtensions": [
         "js",
         "json",
         "ts"
     ],
-    "rootDir": "src",
+
     "testRegex": ".*\\.spec\\.ts$",
     "transform": {
         "^.+\\.(t|j)s$": "ts-jest"
@@ -13,5 +17,11 @@ export default {
         "**/*.(t|j)s"
     ],
     "coverageDirectory": "../coverage",
-    "testEnvironment": "node"
-}
+    "testEnvironment": "node",
+
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix:'<rootDir>/'
+    }),
+};
+
+export default config
